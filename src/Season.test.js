57334546a369f2747cce2c134676700efe074f68
Nicolas Tessier <tessier.nicolas@outlook.fr>
2022-02-10 17:50:39 +0100

    test: 💍 add tests

--- src/Season.test.js
+++ src/Season.test.js
@@ -5,10 +5,23 @@ import "@testing-library/jest-dom";
 import Season from "./Season";
 
 describe(Season.name, () => {
+  beforeAll(() => {
+    jest.useFakeTimers("modern");
+    jest.setSystemTime(new Date(2020, 1, 9));
+  });
   it("renders a icon `❄`", () => {
     render(<Season name="winter" />);
     expect(screen.getByText("❄")).toBeInTheDocument();
   });
-  it("renders a title `Winter`", () => {});
-  it("renders a text `99days`", () => {});
+  it("renders a title `Winter`", () => {
+    render(<Season name="winter" />);
+    expect(screen.getByText("Winter")).toBeInTheDocument();
+  });
+  it("renders a text `99days`", () => {
+    render(<Season name="winter" />);
+    expect(screen.getByText("50 days ago")).toBeInTheDocument();
+  });
+  afterAll(() => {
+    jest.useRealTimers();
+  });
 });
