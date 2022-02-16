96b5ef4e7f42425aa54618d6034e4fc0bc0621ba
Nicolas Tessier <tessier.nicolas@outlook.fr>
2022-02-16 19:56:07 +0100

    Exercice 1 et 2

--- src/App.js
+++ src/App.js
@@ -1,111 +1,39 @@
-import React, { lazy, Suspense, useState } from "react";
-import { isWithinInterval } from "date-fns";
-import { ThemeProvider, createUseStyles } from "react-jss";
+import React from "react";
+import { lazy, useState, Suspense } from "react";
+import { getSeason } from "./utils";
 
-import Season from "./Season";
-import Modal from "./Modal";
-import data from "./data.json";
-import ThemeButton from "./ThemeButton";
+import "./App.css";
+import SeasonCard from "./components/SeasonCard";
+import Button from "./components/Button";
+import Modal from "./components/Modal";
 
-const NextSeason = lazy(() => import("./NextSeason"));
+const NewSeasonCard = lazy(() => import("./components/NewSeasonCard"));
 
-const season = current(data.seasons);
-
-const useStyles = createUseStyles({
-  root: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    fontFamily: "system-ui",
-    height: "100vh",
-    backgroundColor: ({ theme }) => (theme === "light" ? "white" : "black"),
-    color: ({ theme }) => (theme === "light" ? "black" : "white"),
-  },
-  header: {
-    width: "100%",
-    display: "flex",
-    justifyContent: "flex-end",
-  },
-  main: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100%",
-  },
-  content: {
-    marginTop: 8,
-    marginBottom: 8,
-  },
-  actions: {
-    marginTop: 8,
-    marginBottom: 8,
-  },
-});
+const [season, nextSeason] = getSeason();
 
 function App() {
-  const [theme, setTheme] = useState("light");
-  const toggle = () => setTheme(theme === "dark" ? "light" : "dark");
-  const classes = useStyles({ theme });
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
   return (
-    <ThemeProvider theme={{ type: theme, toggle }}>
-      <div className={classes.root}>
-        <header className={classes.header}>
-          <ThemeButton />
-        </header>
-        <main className={classes.main}>
-          <div className={classes.content} data-testid="content">
-            <Season name={season[0]} />
-          </div>
-          <div className={classes.actions} data-testid="actions">
-            <Modal label="Et après ?">
-              <Suspense fallback={<div>Loading...</div>}>
-                <NextSeason name={season[1].next} />
-              </Suspense>
-            </Modal>
-          </div>
-        </main>
-      </div>
-    </ThemeProvider>
+    <div className="app_bg" style={{ backgroundImage: `url(${season.image})` }}>
+      <SeasonCard season={season}>
+        <Button onClick={toggleModal} label="And After ?" />
+      </SeasonCard>
+      {modal && (
+        <Modal>
+          <Suspense fallback={<div>Loading...</div>}>
+            <NewSeasonCard season={nextSeason}>
+              <Button onClick={toggleModal} label="Got it !" />
+            </NewSeasonCard>
+          </Suspense>
+        </Modal>
+      )}
+    </div>
   );
 }
 
 export default App;
-
-export function current(seasons) {
-  return Object.entries(seasons).filter(([name, season]) => {
-    const currentYear = new Date().getFullYear();
-    if (name === "winter") {
-      return (
-        isWithinInterval(new Date(), {
-          start: new Date(
-            currentYear - 1,
-            season.beginAt.month - 1,
-            season.beginAt.day
-          ),
-          end: new Date(currentYear, season.endAt.month - 1, season.endAt.day),
-        }) ||
-        isWithinInterval(new Date(), {
-          start: new Date(
-            currentYear,
-            season.beginAt.month - 1,
-            season.beginAt.day
-          ),
-          end: new Date(
-            currentYear + 1,
-            season.endAt.month - 1,
-            season.endAt.day
-          ),
-        })
-      );
-    }
-    return isWithinInterval(new Date(), {
-      start: new Date(
-        currentYear,
-        season.beginAt.month - 1,
-        season.beginAt.day
-      ),
-      end: new Date(currentYear, season.endAt.month - 1, season.endAt.day),
-    });
-  })[0];
-}
